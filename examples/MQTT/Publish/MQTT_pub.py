42337ec2f0f0a2052676f35aaceaf2674278dbeb
Louiswiz <75520034+min-hs@users.noreply.github.com>
2022-11-16 10:09:10 +0900

    Update MQTT_pub.py

--- examples/MQTT/Publish/MQTT_pub.py
+++ examples/MQTT/Publish/MQTT_pub.py
@@ -1,13 +1,12 @@
 from umqttsimple import MQTTClient
-import time
-import ubinascii
-from machine import Pin,SPI
 from usocket import socket
+from machine import Pin,SPI
 import network
 import rp2
+import time
 
 #mqtt config
-mqtt_server = '192.168.1.11'
+mqtt_server = '192.168.11.2'
 client_id = 'wiz'
 topic_pub = b'hello'
 topic_msg = b'Hello Pico'
@@ -20,12 +19,19 @@ counter = 0
 def w5x00_init():
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
-    nic.ifconfig(('192.168.1.20','255.255.255.0','192.168.1.1','8.8.8.8'))
+    nic.active(True)
+    
+    #None DHCP
+    nic.ifconfig(('192.168.11.20','255.255.255.0','192.168.11.1','8.8.8.8'))
+    
+    #DHCP
+    #nic.ifconfig('dhcp')
+    print('IP address :', nic.ifconfig())
+    
     while not nic.isconnected():
         time.sleep(1)
         print(nic.regs())
-
-#MQTT connect
+    
 def mqtt_connect():
     client = MQTTClient(client_id, mqtt_server, keepalive=60)
     client.connect()
@@ -37,9 +43,10 @@ def reconnect():
     print('Failed to connected to MQTT Broker. Reconnecting...')
     time.sleep(5)
     machine.reset()
-
+    
 def main():
     w5x00_init()
+    
     try:
         client = mqtt_connect()
     except OSError as e:
@@ -50,6 +57,8 @@ def main():
         time.sleep(3)
         
     client.disconnect()
-    
+
 if __name__ == "__main__":
     main()
+
+
