170841010b042c257d5909d72dd4317b75e2d1c2
Louiswiz <75520034+min-hs@users.noreply.github.com>
2022-11-16 09:51:31 +0900

    Update dhcp.py

--- examples/DHCP/dhcp.py
+++ examples/DHCP/dhcp.py
@@ -1,11 +1,30 @@
-import network
+from usocket import socket
 from machine import Pin,SPI
+import network
+import rp2
+import time
 
-spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
-nic = network.WIZNET5K(spi,Pin(17),Pin(20))
+led = Pin(25, Pin.OUT)
 
-# If you use the Dynamic IP(DHCP), you must use the "nic.active(True)".
-# If you use the Static IP, you must use the "nic.ifconfig("IP","subnet","Gateway","DNS")".
-    # nic.ifconfig(('192.168.100.13','255.255.255.0','192.168.100.1','8.8.8.8'))
+#W5x00 chip init
+def w5x00_init():
+    spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
+    nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
+    nic.active(True)
     
-nic.active(True) 
+    #None DHCP
+    #nic.ifconfig(('192.168.11.20','255.255.255.0','192.168.11.1','8.8.8.8'))
+    
+    #DHCP
+    nic.ifconfig('dhcp')
+    
+    print('IP address :', nic.ifconfig())
+    while not nic.isconnected():
+        time.sleep(1)
+        print(nic.regs())
+        
+def main():
+    w5x00_init()
+    
+if __name__ == "__main__":
+    main()
