e5f7d783f2cc7f51983ec400f6ea055cb8e58e42
Louiswiz <75520034+min-hs@users.noreply.github.com>
2022-11-16 10:03:18 +0900

    Update HTTP_Client.py

--- examples/HTTP/HTTP_Client/HTTP_Client.py
+++ examples/HTTP/HTTP_Client/HTTP_Client.py
@@ -1,21 +1,29 @@
-import urequests
+from usocket import socket
 from machine import Pin,SPI
+import urequests
 import network
 import rp2
 import time
+
+led = Pin(25, Pin.OUT)
 
 #W5x00 chip init
 def w5x00_init():
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
-# Use Static IP
-    #nic.ifconfig(('192.168.1.20','255.255.255.0','192.168.1.1','8.8.8.8'))\
-# Use Dynamic IP(DHCP)
     nic.active(True)
+    
+    #None DHCP
+    nic.ifconfig(('192.168.11.20','255.255.255.0','192.168.11.1','8.8.8.8'))
+    
+    #DHCP
+    #nic.ifconfig('dhcp')
+    print('IP address :', nic.ifconfig())
+    
     while not nic.isconnected():
         time.sleep(1)
         print(nic.regs())
-
+    
 def request():
     r = urequests.get('http://httpbin.org/get')
     #r.raise_for_status
@@ -25,7 +33,6 @@ def request():
     if not r:
         print('spreadsheet: no response received')
     print(r.json())
- 
 
 def main():
     w5x00_init()
