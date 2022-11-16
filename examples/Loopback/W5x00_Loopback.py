cd453cac9ee0b47abfec18433e1113da015e100b
Louiswiz <75520034+min-hs@users.noreply.github.com>
2022-11-16 09:54:55 +0900

    Update W5x00_Loopback.py

--- examples/Loopback/W5x00_Loopback.py
+++ examples/Loopback/W5x00_Loopback.py
@@ -1,22 +1,34 @@
-import network
-import time
 from usocket import socket
 from machine import Pin,SPI
+import network
 import rp2
+import time
 
-def init():
+led = Pin(25, Pin.OUT)
+
+#W5x00 chip init
+def w5x00_init():
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
-    nic.ifconfig(('192.168.1.20','255.255.255.0','192.168.1.1','8.8.8.8'))
+    nic.active(True)
+    
+    #None DHCP
+    nic.ifconfig(('192.168.11.20','255.255.255.0','192.168.11.1','8.8.8.8'))
+    
+    #DHCP
+    #nic.ifconfig('dhcp')
+    
+    print('IP address :', nic.ifconfig())
     while not nic.isconnected():
         time.sleep(1)
         print(nic.regs())
-
+    
 def server_loop(): 
     s = socket()
-    s.bind(('192.168.1.20', 5000)) #Source IP Address
+    s.bind(('192.168.11.20', 5000)) #Source IP Address
     s.listen(5)
-            
+    
+    print("TEST server")
     conn, addr = s.accept()
     print("Connect to:", conn, "address:", addr) 
     print("Loopback server Open!")
@@ -28,7 +40,7 @@ def server_loop():
 
 def client_loop():
     s = socket()
-    s.connect(('192.168.1.11', 5000)) #Destination IP Address
+    s.connect(('192.168.11.2', 5000)) #Destination IP Address
     
     print("Loopback client Connect!")
     while True:
@@ -36,11 +48,15 @@ def client_loop():
         print(data.decode('utf-8'))
         if data != 'NULL' :
             s.send(data)
-            
+        
 def main():
-    init()
-    server_loop() #Loopback Server Mode
-    #client_loop() #Loopback client Mode
+    w5x00_init()
+    
+###TCP SERVER###
+    #server_loop()
+
+###TCP CLIENT###
+    client_loop()
 
 if __name__ == "__main__":
     main()
