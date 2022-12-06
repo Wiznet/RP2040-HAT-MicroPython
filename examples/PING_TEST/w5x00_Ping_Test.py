d9cbb5aa20609cae0cf01366872da4091e7cc79f
Louiswiz <75520034+min-hs@users.noreply.github.com>
2022-12-06 13:23:01 +0900

    Update w5x00_Ping_Test.py

--- examples/PING_TEST/w5x00_Ping_Test.py
+++ examples/PING_TEST/w5x00_Ping_Test.py
@@ -7,7 +7,7 @@ def w5x00_init():
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
     nic.active(True)
-    nic.ifconfig(('192.168.11.20','255.255.255.0','192.168.11.1','8.8.8.8'))
+    nic.ifconfig(('192.168.1.20','255.255.255.0','192.168.1.1','8.8.8.8'))
     while not nic.isconnected():
         time.sleep(1)
         print(nic.regs())
